625af6dade2c6d91155b2727a288c6e8bb84d40d
Taras <khomynt97@gmail.com>
2020-08-01 20:19:17 +0300

    7

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //1
-	function getMaxDigit(a){
-		if (isNaN(a)){
+	function getMaxDigit(numbers){
+		if (isNaN(numbers)){
 			alert('Error, its not a number')
 		} else{
 			let maxNumber = 0;
-    const splitNumber = a.split('');
- 		for (i = 0; i < splitNumber.length; i++){
+    const splitNumber = numbers.split('');
+ 		for (let i = 0; i < splitNumber.length; i++){
  		 if (maxNumber < splitNumber[i]) {
       		maxNumber = splitNumber[i];
     	}
@@ -18,12 +18,12 @@ return maxNumber;
 
 //3
 
-function getNameFirstLetterBig (a){
-	a = a.toLowerCase().split('');
-  const bigLetter = (a[0].toUpperCase());
+function getNameFirstLetterBig (name){
+	name = name.toLowerCase().split('');
+  const bigLetter = (name[0].toUpperCase());
 	let eachLetter = '';
-	for (i = 0; i < (a.length) - 1; i++){
-		eachLetter += (a[i + 1] + ''); 
+	for (let i = 0; i < (name.length) - 1; i++){
+		eachLetter += (name[i + 1] + ''); 
 		
     }
   const result = (bigLetter + eachLetter);
@@ -31,45 +31,43 @@ function getNameFirstLetterBig (a){
 		
 }
 //4
-function getSaleryNetto (a) {
+function getSaleryNetto (salery) {
 	const firstTax = 18;
 	const secondTax = 1.5;
 	const taxes = (firstTax + secondTax);
-	const sumOfTaxes = (a * taxes / 100);
-	const saleryNetto = (a - sumOfTaxes);
+	const sumOfTaxes = (salery * taxes / 100);
+	const saleryNetto = (salery - sumOfTaxes);
 	return saleryNetto;
 }
 //5
-function getRandomInt (a, b){
-  const max = +a;
-  const min = +b;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt (max, min){
+  return Math.floor(Math.random() * (+max - (+min) + 1)) + (+min);
 }
 //6
 
-function getCountLetter (a, b){
-	const count = (a.split(b).length - 1);
+function getCountLetter (word, letter){
+	const count = (word.split(letter).length - 1);
 	return count;
 }
 //7
-function convertCurency (a){
+function convertCurency (curency){
 	const dollarsBe = 26.60;
 	const uahBe = 0.036; 
 	let result = 0;
-	if (a.includes('$')){
-		result = (parseInt(a) * dollarsBe + 'UAH');
-	} else if (a.includes('UAH')) {
-		result = (parseInt(a) * uahBe + '$');
+	if (curency.includes('$')){
+		result = (parseInt(curency) * dollarsBe + 'UAH');
+	} else if (curency.includes('UAH')) {
+		result = (parseInt(curency) * uahBe + '$');
 	} else{
-		return ('Error');
+		throw ('Error');
 	}
 	return result;
 	}
 //8
-function getRandomPassword (a) {
-	if (a){
+function getRandomPassword (length) {
+	if (length){
 		let result = '';
-	for (i = 0; i <= a - 1; i++){
+	for (let i = 0; i <= length - 1; i++){
 		const randomNumber = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 		result += String(randomNumber); 
 	}
@@ -77,7 +75,7 @@ function getRandomPassword (a) {
 
 }else {
 	let result = '';
-	for (i = 0; i <= 7; i++){
+	for (let i = 0; i <= 7; i++){
 		const randomNumber = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 		result += String(randomNumber); 
 	}
@@ -85,26 +83,26 @@ function getRandomPassword (a) {
 }
 }
 //9
-function deleteLetter(a){
-  const letters = a.split('');
+function deleteLetter(word, letter){
+  const letters = word.split('');
   let result = '';
-  for (i = 0; i < a.length; i++){
-    if (a[i] !== 'a'){
-      result += a[i];
+  for (let i = 0; i < word.length; i++){
+    if (word[i] !== letter){
+      result += word[i];
     }
   }
   return result;
 }
 //10
-function isPalyndrom(a){
-  const split = a.split('');
+function isPalyndrom(word){
+  const split = word.split('');
   const length = split.length;
 
-  for (i = 0; i <= split.length / 2; i++){
+  for (let i = 0; i <= split.length / 2; i++){
     if (split[i] === split[length - (i + 1)]){
-    return 'true';
+    return true;
   } else {
-    return 'false';
+    return false;
   }
   }
   
@@ -120,7 +118,7 @@ container.innerHTML=`
 <p>Count of latters: ${getCountLetter(prompt('Write a word'), prompt('Write a letter'))};</p>
 <p>Converted curency: ${convertCurency(prompt('Write a sum'))};</p>
 <p>Generated password: ${getRandomPassword(prompt('How lond the password should be. Write a number'))};</p>
-<p>Word without 'a': ${deleteLetter(prompt('Write a word you want the (a) to be deleted'))};</p>
+<p>Word without chosen letter: ${deleteLetter(prompt('Write a word'), prompt('Write the letter you want be deleted'))};</p>
 <p>Is your word palyndrom?: ${isPalyndrom(prompt('Write your word, you want to chack on palyndrom'))};</p>
 `;
 
